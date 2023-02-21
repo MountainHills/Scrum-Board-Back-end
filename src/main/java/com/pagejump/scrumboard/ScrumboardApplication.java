0627970ab79318a694e113b9e3a1c99176bedbca
MountainHills <antonbondoc14@gmail.com>
2023-02-22 07:40:12 +0800

    first backend commit

---
+++ src/main/java/com/pagejump/scrumboard/ScrumboardApplication.java
@@ -0,0 +1,13 @@
+package com.pagejump.scrumboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pagejump/scrumboard/ScrumboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagejump.scrumboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
