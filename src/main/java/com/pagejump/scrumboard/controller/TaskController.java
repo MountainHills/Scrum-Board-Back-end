670c8ae289000fdfd40d80d6686622e0db21589f
MountainHills <antonbondoc14@gmail.com>
2023-03-04 23:35:56 +0800

    Fixed logging statements

--- src/main/java/com/pagejump/scrumboard/controller/TaskController.java
+++ src/main/java/com/pagejump/scrumboard/controller/TaskController.java
@@ -24,28 +24,27 @@ public class TaskController {
     @GetMapping
     public List<Task> getAllTasks(
             @RequestParam(value = "isDeleted", required = false, defaultValue = "false") boolean isDeleted) {
-        log.info("Getting all tasks where deleted = " + isDeleted);
+        log.info("Getting all tasks where deleted = {}", isDeleted);
         return taskService.getAllTasks(isDeleted);
     }
 
     // View task by ID.
     @GetMapping(path = "{taskId}")
     public Task getTaskById(@PathVariable("taskId") Long taskId) {
-        log.info("Getting task with id = " + taskId);
+        log.info("Getting task with id = {}", taskId);
         return taskService.getTaskById(taskId);
     }
 
     // For inserting new tasks.
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody @Valid TaskRequestDTO task) {
-        log.info("Inserting task with the following information: title = " + task.getTitle()
-        + ", description = " + task.getDescription());
+        log.info("Inserting task with the following information title = {}, description = {}", task.getTitle(), task.getDescription());
         return new ResponseEntity<>(taskService.createTask(task), HttpStatus.CREATED);
     }
 
     @DeleteMapping(path = "{taskId}")
     public ResponseEntity<Task> deleteTask(@PathVariable("taskId") Long taskId) {
-        log.info("Soft deleting task with id = " + taskId);
+        log.info("Soft deleting task with id = {}", taskId);
         taskService.deleteTask(taskId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
@@ -54,8 +53,7 @@ public class TaskController {
     @PutMapping(path = "{taskId}")
     public ResponseEntity<Task> updateTask(@PathVariable("taskId") Long taskId,
                               @RequestBody @Valid TaskRequestDTO update) {
-        log.info("Updating task id #" + taskId + " with the following information: " + update.toString());
+        log.info("Updating task id = {} with the following information: {}", taskId, update.toString());
         return new ResponseEntity<>(taskService.updateTask(taskId, update), HttpStatus.ACCEPTED);
-
     }
 }
